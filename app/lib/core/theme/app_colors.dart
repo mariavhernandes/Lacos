a21bb2fac2f43757d59bc40f67116311b0f06c16
mariavhernandes <mariavictoriachernandes@gmail.com>
2026-07-17 18:16:46 -0300

    feat: configura estrutura base do projeto

---
+++ app/lib/core/theme/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Cores principais
+  static const Color primary = Color(0xFF033B63);
+  static const Color primaryLight = Color(0xFF466A99);
+  static const Color secondary = Color(0xFF9ED1FF);
+
+  // Backgrounds
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color card = Color(0xFFF5F5F5);
+
+  // Textos
+  static const Color textPrimary = Color(0xFF033B63);
+  static const Color textSecondary = Color(0xFF466A99);
+
+  // Estados
+  static const Color success = Color(0xFF4CAF50);
+  static const Color warning = Color(0xFFFFC107);
+  static const Color error = Color(0xFFE53935);
+
+  // Outros
+  static const Color divider = Color(0xFFE0E0E0);
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'core/routes/app_routes.dart';
 
-import 'app.dart';
 import 'firebase_options.dart';
+import 'core/theme/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,5 +12,39 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const App());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Laços',
+      theme: AppTheme.lightTheme,
+
+      initialRoute: AppRoutes.home,
+      routes: AppRoutes.routes,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Laços'),
+      ),
+      body: const Center(
+        child: Text(
+          'Firebase conectado com sucesso!',
+        ),
+      ),
+    );
+  }
 }
